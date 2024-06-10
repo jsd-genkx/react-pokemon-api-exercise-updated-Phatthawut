051fab01153761810e007074dcfc6963a60b52c1
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-06-10 12:20:51 +0700

    clear git

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import PokemonBasicFetch from "./components/PokemonBasicFetch";
-import PokemonBasicFetchAxios from "./components/PokemonBasicFetchAxios";
-import PokemonFetch from "./components/PokemonFetch";
-import PokemonFetchAxios from "./components/PokemonFetchAxios";
-import PokemonSearchFetch from "./components/PokemonSearchFetch";
+// import PokemonBasicFetchAxios from "./components/PokemonBasicFetchAxios";
+// import PokemonFetch from "./components/PokemonFetch";
+// import PokemonFetchAxios from "./components/PokemonFetchAxios";
+// import PokemonSearchFetch from "./components/PokemonSearchFetch";
 
 const App = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      {/* Fetch API */}
-      <PokemonBasicFetch />
-      {/* <PokemonFetch /> */}
-      {/* Axios */}
-      {/* <PokemonBasicFetchAxios /> */}
-      {/* <PokemonFetchAxios /> */}
-      {/* Bonus Code Example */}
-      {/* <PokemonSearchFetch /> */}
-      {/* Bonus Code Challenge: refactor PokemonSearchFetch to use Axios */}
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+			{/* Fetch API */}
+			<PokemonBasicFetch />
+			{/* <PokemonFetch /> */}
+			{/* Axios */}
+			{/* <PokemonBasicFetchAxios /> */}
+			{/* <PokemonFetchAxios /> */}
+			{/* Bonus Code Example */}
+			{/* <PokemonSearchFetch /> */}
+			{/* Bonus Code Challenge: refactor PokemonSearchFetch to use Axios */}
+		</div>
+	);
 };
 
 export default App;
--- src/components/PokemonBasicFetch.jsx
+++ src/components/PokemonBasicFetch.jsx
@@ -1,40 +1,48 @@
 import { useEffect, useState } from "react";
 
 const PokemonBasicFetch = () => {
-  const [pokemonData, setPokemonData] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+	const [pokemonData, setPokemonData] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        // fetch data from api
+	useEffect(() => {
+		const fetchPokemon = async () => {
+			try {
+				// fetch data from api
+				const response = await fetch(
+					"https://pokeapi.co/api/v2/pokemon?limit=10"
+				);
+				// handle data
+				const data = await response.json();
+				setPokemonData(data.results);
+			} catch (error) {
+				setError(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+		// invoke function
+		fetchPokemon();
+	}, []);
 
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-        // handle data
+	if (error) {
+		return <div>Error: {error}</div>;
+	}
 
-    // invoke function
-
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  return (
-    <div>
-      <h1 className="font-bold">Pokémon List</h1>
-      <ul>
-        {pokemonData.map((pokemon, index) => (
-          <li key={index}>{pokemon.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="font-bold">Pokémon List</h1>
+			<ul>
+				{pokemonData.map((pokemon, index) => (
+					<li key={index}>{pokemon.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default PokemonBasicFetch;
